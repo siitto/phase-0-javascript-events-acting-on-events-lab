2eb417a1e40e08872e00ebc9289dda01e61a6795
+ <+>
2023-11-03 00:18:27 -0400

    created code to move dodger to the right

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+ dodger = document.getElementById("dodger"); 
+
+ let left = dodger.style.left;
+ left = parseInt(left);
+ moveDodgerRight();
+
+function moveDodgerRight(){
+let newPosition = dodger.style.left.replace("px", "");
+  let left = parseInt(newPosition, 10);
+
+if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+      }    
+    }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
